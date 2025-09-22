1821d4b23ec97300a56d420cc5415760b8ccee1e
ss <2chashao@gmail.com>
2025-09-23 01:09:11 +0800

    homework5

--- Problem Sets/Problem Set 5/main.cu
+++ Problem Sets/Problem Set 5/main.cu
@@ -5,6 +5,7 @@
 #include "utils.h"
 #include "timer.h"
 #include <cstdio>
+#include <vector>
 #if defined(_WIN16) || defined(_WIN32) || defined(_WIN64)
 #define NOMINMAX
 #include <Windows.h>
@@ -22,6 +23,11 @@ void computeHistogram(const unsigned int *const d_vals,
                       unsigned int* const d_histo,
                       const unsigned int numBins,
                       const unsigned int numElems);
+void computeHistogram2(const unsigned int* const d_vals,
+    unsigned int* d_intermediate_histo,
+    unsigned int* const d_histo,
+    const unsigned int numBins,
+    const unsigned int numElems);
 
 int main(void)
 {
@@ -29,9 +35,10 @@ int main(void)
   const unsigned int numElems = 10000 * numBins;
   const float stddev = 100.f;
 
-  unsigned int *vals = new unsigned int[numElems];
+  std::vector<unsigned int> vals(numElems);
   unsigned int *h_vals = new unsigned int[numElems];
-  unsigned int *h_studentHisto = new unsigned int[numBins];
+  std::vector<unsigned int> h_studentHisto(numBins);
+  std::vector<unsigned int> h_studentHisto2(numBins);
   unsigned int *h_refHisto = new unsigned int[numBins];
 
 #if defined(_WIN16) || defined(_WIN32) || defined(_WIN64)
@@ -59,42 +66,55 @@ int main(void)
     vals[i] = std::min((unsigned int) std::max((int)normalDist(rng), 0), numBins - 1);
   }
 
-  unsigned int *d_vals, *d_histo;
+  unsigned int *d_vals, *d_histo, *d_histo2;
 
   GpuTimer timer;
-
+  size_t NUM_PARTS = 1024;
+  size_t gridX = (numElems + NUM_PARTS - 1) / NUM_PARTS;
+  unsigned int* d_intermediate_histo;
+  size_t NumIntermediate = gridX * NUM_PARTS;
+  checkCudaErrors(cudaMalloc(&d_intermediate_histo, NumIntermediate * sizeof(unsigned int)));
   checkCudaErrors(cudaMalloc(&d_vals,    sizeof(unsigned int) * numElems));
   checkCudaErrors(cudaMalloc(&d_histo,   sizeof(unsigned int) * numBins));
   checkCudaErrors(cudaMemset(d_histo, 0, sizeof(unsigned int) * numBins));
-
-  checkCudaErrors(cudaMemcpy(d_vals, vals, sizeof(unsigned int) * numElems, cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMalloc(&d_histo2, sizeof(unsigned int) * numBins));
+  checkCudaErrors(cudaMemset(d_histo2, 0, sizeof(unsigned int) * numBins));
+  checkCudaErrors(cudaMemcpy(d_vals, vals.data(), sizeof(unsigned int) * numElems, cudaMemcpyHostToDevice));
 
   timer.Start();
   computeHistogram(d_vals, d_histo, numBins, numElems);
   timer.Stop();
-  int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
+  int err = printf("Your code1 ran in: %f msecs.\n", timer.Elapsed());
 
   if (err < 0) {
     //Couldn't print! Probably the student closed stdout - bad news
     std::cerr << "Couldn't print timing information! STDOUT Closed!" << std::endl;
     exit(1);
   }
-
+  
   // copy the student-computed histogram back to the host
-  checkCudaErrors(cudaMemcpy(h_studentHisto, d_histo, sizeof(unsigned int) * numBins, cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(h_studentHisto.data(), d_histo, sizeof(unsigned int) * numBins, cudaMemcpyDeviceToHost));
 
   //generate reference for the given mean
-  reference_calculation(vals, h_refHisto, numBins, numElems);
-
+  reference_calculation(vals.data(), h_refHisto, numBins, numElems);
   //Now do the comparison
-  checkResultsExact(h_refHisto, h_studentHisto, numBins);
+  checkResultsExact(h_refHisto, h_studentHisto.data(), numBins);
+
+  GpuTimer timer2;
+  timer2.Start();
+  computeHistogram2(d_vals, d_intermediate_histo, d_histo2, numBins, numElems);
+  timer2.Stop();
+  err = printf("Your code2 ran in: %f msecs.\n", timer2.Elapsed());
+  checkCudaErrors(cudaMemcpy(h_studentHisto2.data(), d_histo2, sizeof(unsigned int) * numBins, cudaMemcpyDeviceToHost));
+  checkResultsExact(h_refHisto, h_studentHisto2.data(), numBins);
+
 
   delete[] h_vals;
   delete[] h_refHisto;
-  delete[] h_studentHisto;
 
   cudaFree(d_vals);
   cudaFree(d_histo);
-
+  cudaFree(d_histo2);
+  checkCudaErrors(cudaFree(d_intermediate_histo));
   return 0;
 }
--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -26,7 +26,13 @@
 
 
 #include "utils.h"
+#include <vector>
 #define NUM_BLOCK_X 256
+#define NUM_BINS 1024
+#define NUM_PARTS 1024
+
+
+// 使用Atomic操作的直方图计算
 __global__ void AtomicHistogram(const unsigned int* const vals, //INPUT
 	unsigned int* const histo,      //OUPUT
 	int numVals)
@@ -39,23 +45,106 @@ __global__ void AtomicHistogram(const unsigned int* const vals, //INPUT
 	}
 }
 
+__global__ void LocalHistogram(const unsigned int* const vals, //INPUT
+	unsigned int* const histo,      //OUPUT
+	int numVals)
+{
+	__shared__ unsigned int localHisto[NUM_BINS];
+
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int tid = threadIdx.x;
+
+	// Initialize shared memory histogram
+	if (tid < NUM_BINS)
+		localHisto[tid] = 0;
+	__syncthreads();
+
+	// Build local histogram
+	if (i < numVals)
+	{
+		atomicAdd(&localHisto[vals[i]], 1);
+	}
+
+	__syncthreads();
+
+	// Write back to global memory
+	if (tid < NUM_BINS)
+	{
+		atomicAdd(&histo[blockIdx.x * NUM_BINS + tid], localHisto[tid]);
+	}
+}
+
+__global__ void MergeHistogram(const unsigned int* const intermediateHisto, //INPUT
+	unsigned int* const histo,      //OUPUT
+	int numParts)
+{
+	int bin = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (bin < NUM_BINS)
+	{
+		//printf("bin: %d,", bin);
+		unsigned int sum = 0;
+		for (int i = 0; i < numParts; ++i)
+		{
+			int pos = i * NUM_BINS + bin;
+			sum += intermediateHisto[pos];
+		}
+		histo[bin] = sum;
+	}
+}
+
+template<typename Type>
+void PrintDeviceMemory(size_t numElems, Type* deviceVals, const std::string& name, size_t offset = 0)
+{
+	std::vector<Type> h_inputVals;
+	h_inputVals.resize(numElems);
+	checkCudaErrors(cudaMemcpy(h_inputVals.data(), deviceVals, sizeof(Type) * numElems, cudaMemcpyDeviceToHost));
+	std::cout << name << ":";
+	for (int i = offset; i < numElems; i++)
+	{
+		std::cout << h_inputVals[i] << ", ";
+	}
+	std::cout << std::endl;
+}
 
 void computeHistogram(const unsigned int* const d_vals, //INPUT
                       unsigned int* const d_histo,      //OUTPUT
                       const unsigned int numBins,
                       const unsigned int numElems)
 {
-	//TODO Launch the yourHisto kernel
+	size_t blockX = NUM_BLOCK_X;
+	size_t gridX = (numElems + blockX - 1) / blockX;
+	const dim3 blockSize(blockX, 1, 1);
+	const dim3 gridSize(gridX, 1, 1);
+	AtomicHistogram << <gridSize, blockSize >> > (d_vals, d_histo, numElems);
+	cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+	//PrintDeviceMemory(NUM_BINS, d_histo, "Result0:");
+}
 
-	//if you want to use/launch more than one kernel,
-	//feel free
+
+void computeHistogram2(const unsigned int* const d_vals, //INPUT
+	unsigned int* d_intermediate_histo,
+	unsigned int* const d_histo,      //OUTPUT
+	const unsigned int numBins,
+	const unsigned int numElems)
+{
+	size_t gridX = (numElems + NUM_PARTS - 1) / NUM_PARTS;
 
 	{
-		size_t blockX = NUM_BLOCK_X;
-		size_t gridX = (numElems + blockX - 1) / blockX;
-		const dim3 blockSize(blockX, 1, 1);
+		const dim3 blockSize(NUM_PARTS, 1, 1);
 		const dim3 gridSize(gridX, 1, 1);
-		AtomicHistogram << <gridSize, blockSize >> > (d_vals, d_histo, numElems);
+		LocalHistogram << <gridSize, blockSize >> > (d_vals, d_intermediate_histo, numElems);
+		cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+		//PrintDeviceMemory(NumIntermediate, d_intermediate_histo, "Intermediate:", (gridX - 1) * NUM_BINS);
 	}
-	cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+	{
+		gridX = 32;
+		const dim3 blockSize(gridX, 1, 1);
+		const dim3 gridSize((numBins + gridX - 1) / gridX, 1, 1);
+		MergeHistogram << <gridSize, blockSize >> > (d_intermediate_histo, d_histo, gridX);
+		cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+	}
+	//PrintDeviceMemory(NUM_BINS, d_histo, "Result1:");
+	
 }
